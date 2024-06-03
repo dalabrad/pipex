e59f6393e527c758f1cfbd0715243119c451c371
David <dalabrad@student.42urduliz.com>
2024-06-03 17:39:51 +0200

    modified main.c

--- main.c
+++ main.c
@@ -6,14 +6,26 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 10:57:55 by dalabrad          #+#    #+#             */
-/*   Updated: 2024/06/03 10:59:48 by dalabrad         ###   ########.fr       */
+/*   Updated: 2024/06/03 16:19:34 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 
-int main(void)
+int	main(int argc, char **argv)
 {
-    ft_printf("pipex\n");
-    return (0);
+	int i;
+
+	if (argc != 5)
+	{
+		write(2, "Error\n", 7);
+		return (1);
+	}
+	i = 0;
+	while (argv[i])
+	{
+		ft_printf("arg[%d] : '%s'\n", i, argv[i]);
+		i++;
+	}
+	return (0);
 }
