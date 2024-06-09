2751d72fe4a4cbf5c940c0003db553155b46392a
David <dalabrad@student.42urduliz.com>
2024-06-09 17:19:25 +0200

    modified pipex_error_msg.c

--- pipex_error_msg.c
+++ pipex_error_msg.c
@@ -6,7 +6,7 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 16:08:55 by dalabrad          #+#    #+#             */
-/*   Updated: 2024/06/09 12:24:13 by dalabrad         ###   ########.fr       */
+/*   Updated: 2024/06/09 16:57:14 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 int	error_msg(char *str)
 {
 	write(2, str, ft_strlen(str));
+	return (1);
+}
+
+int	cmd_error_msg(char *cmd)
+{
+	ft_putstr_fd("Command '", 2);
+	ft_putstr_fd(cmd, 2);
+	ft_putstr_fd("' not  found.\n", 2);
 	return (1);
 }
 
