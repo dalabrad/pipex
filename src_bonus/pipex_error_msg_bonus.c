cdcbe4512c0479e0cb5712501cb5325a89121e51
David <dalabrad@student.42urduliz.com>
2024-06-17 13:54:48 +0200

    modified the error string for invalid arguments in bonus

--- src_bonus/pipex_error_msg_bonus.c
+++ src_bonus/pipex_error_msg_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 11:07:05 by dalabrad          #+#    #+#             */
-/*   Updated: 2024/06/17 13:28:26 by dalabrad         ###   ########.fr       */
+/*   Updated: 2024/06/17 13:53:34 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ void	pxb_error_msg(int err)
 	if (err == INV_ARGS)
 	{
 		ft_putstr_fd("invalid arguments, use: ", 2);
-		ft_putstr_fd("./pipex infile cmd1 cmd2 ... comandon outfile or ", 2);
-		ft_putstr_fd("./pipex here_doc LIMITER cmd1 cmd2 outfile", 2);
+		ft_putstr_fd("./pipex infile cmd1 cmd2 ... cmdn outfile or ", 2);
+		ft_putstr_fd("./pipex here\\_doc LIMITER cmd1 cmd2 outfile\n", 2);
 	}
 	else if (err == ERR_HEREDOC)
-		ft_putstr_fd("error opening here_doc", 2);
+		ft_putstr_fd("error opening here_doc\n", 2);
 	else if (err == ERR_RDHEREDOC)
-		ft_putstr_fd("error reading from here_doc", 2);
+		ft_putstr_fd("error reading from here_doc\n", 2);
 }
 
 void	pxb_perror_exit(int err)
