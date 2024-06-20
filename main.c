a077da79f555c1469d816c73e8d9645aa3bfdb2d
David Labrador Gracia <dalabrad@c3r6s2.42urduliz.com>
2024-06-20 12:47:05 +0200

    modified mian.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 10:57:55 by dalabrad          #+#    #+#             */
-/*   Updated: 2024/06/14 10:59:54 by dalabrad         ###   ########.fr       */
+/*   Updated: 2024/06/20 12:45:43 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int	main(int argc, char **argv, char **envp)
 		px_perror_exit(NULL, INV_ARGS);
 	pipex.in_fd = open(argv[1], O_RDONLY);
 	pipex.out_fd = open(argv[4], O_TRUNC | O_CREAT | O_WRONLY, 0000644);
+	pipex.paths_array = pipex_path_array(envp);
 	if (pipe(pipex.fd_pipe) < 0)
 		px_perror_exit(NULL, PIPE_ERR);
-	pipex.paths_array = pipex_path_array(envp);
 	pipex.pid1 = fork();
 	if (pipex.pid1 == -1)
 		px_perror_exit(NULL, FORK_ERR);
